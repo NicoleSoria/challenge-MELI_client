ec8b76649615bfffd4fbc0f3a517213cbbba331b
Tamara Soria <tamarasoria35@outlook.com.ar>
2022-03-13 20:16:49 -0300

    correccion de loading

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
 import { CategoriesObsService } from '../../services/categories-obs.service';
 import { IProducts } from '../../models/product.model';
+import { SpinnerObsService } from '../../services/spinner-obs.service';
 
 @Component({
   selector: 'app-products-list',
@@ -13,16 +14,19 @@ export class ProductsListComponent implements OnInit {
 
   query: string;
   products: IProducts[] = [];
+  isLoading: boolean = true;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private productService: ProductService,
-              private categoriesObsService: CategoriesObsService,
-              private router: Router) { }
+    private productService: ProductService,
+    private categoriesObsService: CategoriesObsService,
+    private router: Router,
+    private spinnerObsService: SpinnerObsService) { }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((resp) => {
       this.query = resp.search;
       this.getItems();
+      this.showEmptySatate();
     });
   }
 
@@ -35,6 +39,12 @@ export class ProductsListComponent implements OnInit {
 
   openProduct(idProduct: string) {
     this.router.navigate([`items/${idProduct}`]);
+  }
+
+  showEmptySatate() {
+    this.spinnerObsService.getValue().subscribe((resp) => {
+      this.isLoading = resp;
+    });
   }
 
 }
